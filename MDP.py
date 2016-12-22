db384aa76e2a34565e778d8b0759c92aa0516041
GeoffNN <geoffrey.negiar@polytechnique.edu>
2016-12-22 01:38:20 +0100

    Added terminal states ; implemented EasyMaze as a MDP subclass

--- MDP.py
+++ MDP.py
@@ -3,23 +3,24 @@ from scipy.stats import rv_discrete
 from policy import Policy
 
 class MDP:
-    def __init__(self, states, actions, dynamics, rewards, gamma):
+    def __init__(self, states, actions, dynamics, rewards, term_states, gamma):
         self.states = states
         self.actions = actions
         # dynamics[state, action] is the list of probabilities for next_state
-        assert dynamics.shape == (len(states), len(actions))
+        assert dynamics.shape == (len(states), len(actions), len(states))
         self.dynamics = dynamics
         # rewards[state] is deterministic, as in the paper
         assert len(rewards) == len(states)
         self.rewards = rewards
+        self.term_states = term_states
         self.gamma = gamma
 
     def simulate(self, x_0, policy, n_trajectories=100, trajectory_length=1000):
         trajectories = np.zeros((n_trajectories, trajectory_length))
         rewards = np.zeros((n_trajectories, trajectory_length))
         state = x_0
-        for k in n_trajectories:
-            for j in trajectory_length:
+        for k in range(n_trajectories):
+            for j in range(trajectory_length):
                 trajectories[k, j] = state
                 action = policy.next_action(state)
                 next_state, rew = self.simulate_next_state(state, action)
@@ -28,9 +29,12 @@ class MDP:
         return trajectories, rewards
 
     def simulate_next_state(self, state, action):
+        if state in self.term_states:
+            return state, 0
         rand_var = rv_discrete(values=(self.states, self.dynamics[state, action]))
         next_state = rand_var.rvs()
         reward = self.rewards[next_state]
+
         return next_state, reward
 
     def bellman_operator(self, policy, values):
--- problems.py
+++ problems.py
@@ -1,32 +1,61 @@
 """Here we define the MDPs on which we test our algorithms."""
 from MDP import MDP
 import numpy as np
+from policy import Policy
+import seaborn as sb
 
 
-def easy_maze_init(side=5, gamma=.9):
-    n_squares = side ** 2
-    states = np.array(range(n_squares))
-    left = 0
-    right = 1
-    up = 2
-    down = 3
-    actions = [left, right, up, down]
-    n_actions = len(actions)
-    dynamics = np.zeros((n_squares, n_actions))
-    for k in range(n_squares):
-        dynamics[k, up] = np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (-1, 0), (side, side),
-                                               'clip')
-        dynamics[k, down] = np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (1, 0), (side, side),
-                                                 'clip')
-        dynamics[k, right] = np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (0, 1), (side, side),
-                                                  'clip')
-        dynamics[k, left] = np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (0, -1), (side, side),
-                                                 'clip')
-    # Single reward in last square
-    rewards = np.zeros(n_squares)
-    rewards[n_squares - 1] = 100
+class EasyMaze(MDP):
+    def __init__(self, side=5, gamma=.9):
+        self.n_squares = side ** 2
+        self.side = side
+        states = np.array(range(self.n_squares))
+        self.left = 0
+        self.right = 1
+        self.up = 2
+        self.down = 3
+        actions = [self.left, self.right, self.up, self.down]
+        n_actions = len(actions)
+        dynamics = np.zeros((self.n_squares, n_actions, self.n_squares))
+        for k in range(self.n_squares):
+            dynamics[k, self.up] = [
+                1 if j == np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (-1, 0), (side, side),
+                                               'clip') else 0 for j in states]
+            dynamics[k, self.down] = [
+                1 if j == np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (1, 0), (side, side),
+                                               'clip') else 0 for j in states]
+            dynamics[k, self.right] = [
+                1 if j == np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (0, 1), (side, side),
+                                               'clip') else 0 for j in states]
+            dynamics[k, self.left] = [
+                1 if j == np.ravel_multi_index(np.array(np.unravel_index(k, (side, side))) + (0, -1), (side, side),
+                                               'clip') else 0 for j in states]
+        # Single reward in last square
+        rewards = np.zeros(self.n_squares)
+        rewards[self.n_squares - 1] = 100
+        super().__init__(states, actions, dynamics, rewards, [self.n_squares-1], gamma)
 
-    return states, actions, dynamics, rewards, gamma
 
 
-Easy_Maze = MDP(*easy_maze_init())
+
+def plot_Easy_Maze_trajectory(maze, trajectory):
+    agg = np.zeros((len(maze.states), len(maze.states)))
+    for i in maze.states:
+        agg[np.unravel_index(i, (maze.side, maze.side))] = np.sum(trajectory[trajectory == i])
+    sb.heatmap(agg)
+
+
+Easy_Maze = EasyMaze()
+
+simple_pol_arr = np.zeros(len(Easy_Maze.states))
+for x in Easy_Maze.states:
+    j, k = np.unravel_index(x, (Easy_Maze.side, Easy_Maze.side))
+    if j >= k:
+        simple_pol_arr[x] = Easy_Maze.right
+    else:
+        simple_pol_arr[x] = Easy_Maze.down
+
+simple_pol = Policy(simple_pol_arr)
+
+
+Easy_Maze.simulate_next_state(19, Easy_Maze.down)
