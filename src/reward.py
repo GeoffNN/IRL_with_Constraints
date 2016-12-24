06ff7a70f9baec3e17083c05f85b0205f1fbe0bb
GeoffNN <geoffrey.negiar@polytechnique.edu>
2016-12-24 17:27:05 +0100

    Modified Notebook

--- src/reward.py
+++ src/reward.py
@@ -3,6 +3,7 @@ import numpy as np
 
 class RewardFunction:
     """Describe a reward function as a linear combination of function in a given basis"""
+
     # basis should not be changed, but weight can be.
     # basis are (dim_approx x states) matrix
     # weights are (dim_approx) array
@@ -23,6 +24,8 @@ class RewardFunction:
                     gamma, decay rate of value over time
                     x_0 starting state
         """
-        V = np.array([np.sum(np.array([gamma ** k * self.basis[i][state] for (k, state) in np.ndenumerate(trajectories)])) for i in range(self.dim_approx)])
+        V = np.array(
+                [np.sum(np.array([gamma ** k * self.basis[i][state] for (k, state) in np.ndenumerate(trajectories)]))
+                 for i in range(self.dim_approx)])
         V = np.dot(self.weights, V)
-return V
+        return V
